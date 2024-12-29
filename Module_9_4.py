2832ca7632b047e32fac6c7e33e5b1a62a43c594
ExcelencCode <r.sykorski@gmail.com>
2024-12-29 17:06:50 +0500

    Update Module_9_4.py

--- Module_9_4.py
+++ Module_9_4.py
@@ -10,7 +10,7 @@ def get_advanced_writer(file_name):
     def write_everything(*data_set):
         with open(file_name, 'a', encoding='utf-8') as file:
             for item in data_set:
-                file.write(repr(item) + '\n')
+                file.write(str(item) + '\n')
 
     return write_everything
 
@@ -21,7 +21,7 @@ write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
 
 
-# Метод __сфдд__
+# Метод __call__
 from random import choice
 
 
